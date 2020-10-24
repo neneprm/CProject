7d2fe6bc071751ece85c10d844bde819bc0152c7
neneprm <s.prima@hotmai.com>
2020-10-25 00:56:40 +0700

    Add bird

--- main.c
+++ main.c
@@ -26,14 +26,14 @@ void unloadTexture()
 
 int main()
 {
-    const int screenWidth = 288;
-    const int screenHeight = 510;
+    const int screenWidth = 490;
+    const int screenHeight = 735;
     InitWindow(screenWidth, screenHeight, "Flappy Bird");
 
     float scrollingBack = 0.0f;
     float scrollingFore = 0.0f;
 
-    Vector2 position = {144.0f, 250.0f};
+    Vector2 position = {215.0f,362.5f};
     Rectangle  frameRec = {0.0f, 0.0f, (float)bird.width/3, (float)bird.height};
     int currentFrame = 0;
 
@@ -47,36 +47,34 @@ int main()
     {
         scrollingBack -= 0.1f;
         scrollingFore -= 3.0f;
-
-        if (scrollingBack <= -(float )background.width*2) scrollingBack = 0;
-        if (scrollingFore <= -(float )foreground.width*2) scrollingFore = 0;
+        if (scrollingBack <= -(float)background.width*2) scrollingBack = 0;
+        if (scrollingFore <= -(float)foreground.width*2) scrollingFore = 0;
 
         framesCounter++;
-
         if (framesCounter >= (60/framesSpeed))
         {
             framesCounter = 0;
             currentFrame++;
 
-            if (currentFrame > 5) currentFrame = 0;
+            if (currentFrame > 2) currentFrame = 0;
 
-            frameRec.x = (float)currentFrame*(float)bird.width/6;
+            frameRec.x = (float)currentFrame*(float)bird.width/3;
         }
+
 
         BeginDrawing();
             ClearBackground(RAYWHITE);
 
-//            DrawTextureEx(background, (struct Vector2){scrollingBack, 0}, 0.0f, 2.0f, WHITE);
-//            DrawTextureEx(background, (struct Vector2){(float)background.width*2 + scrollingBack, 0}, 0.0f, 2.0f, WHITE);
-            DrawTextureEx(bird, (struct Vector2){144, 250}, 0.0f, 0.0f, WHITE);
-            DrawRectangleLines(144, 250, bird.width, bird.height, LIME);
-            DrawRectangleLines(144 + frameRec.x, 250 + frameRec.y, frameRec.width, frameRec.height, RED);
+            DrawTextureEx(background, (struct Vector2){scrollingBack, -500}, 0.0f, 2.5f, WHITE);
+            DrawTextureEx(background, (struct Vector2){(float)background.width*2 + scrollingBack, -500}, 0.0f, 2.5f, WHITE);
 
-//            DrawTextureEx(foreground, (struct Vector2){scrollingFore, 440}, 0.0f, 2.0f, WHITE);
-//            DrawTextureEx(foreground, (struct Vector2){(float)foreground.width*2 + scrollingFore, 440}, 0.0f, 2.0f, WHITE);
-            DrawTextureRec(bird, frameRec, position, WHITE);
+            DrawTextureEx(foreground, (struct Vector2){scrollingFore, 630}, 0.0f, 2.5f, WHITE);
+            DrawTextureEx(foreground, (struct Vector2){(float)foreground.width*2 + scrollingFore, 630}, 0.0f, 2.5f, WHITE);
+
+            DrawTextureRec(bird, (Rectangle){currentFrame * (bird.width / 3),0,bird.width/3,bird.height}, position, WHITE);
 
         EndDrawing();
+
     }
 
     unloadTexture();
