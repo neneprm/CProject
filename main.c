931fbff201274d9669ca83342af7a9a67ae15a85
neneprm <s.prima@hotmai.com>
2020-11-12 17:35:17 +0700

    Add text and sound effects

--- main.c
+++ main.c
@@ -54,6 +54,14 @@ typedef struct Pipe
 
 } Pipe;
 
+typedef struct Effect
+{
+    Sound hit;
+    Sound jump;
+    Sound point;
+
+} Effect;
+
 //------------------------------------------------------------------------------------
 // Structure variable
 //------------------------------------------------------------------------------------
@@ -61,18 +69,23 @@ typedef struct Pipe
 static Map map;
 static Bird bird;
 static Pipe pipe[MAX_PIPES];
+static Effect effect;
 
 //------------------------------------------------------------------------------------
 // Global Variables Declaration
 //------------------------------------------------------------------------------------
 
+bool gameStart;
 bool gameOver;
+int gameRun;
 
 Texture2D gameOverSprite;
 Texture2D scoreBoard;
+Texture2D title;
 
-static int score = 0;
-static int hiScore = 0;
+static int score;
+static int hiScore;
+static float speed;
 
 static const int screenWidth = 490;
 static const int screenHeight = 735;
@@ -95,6 +108,8 @@ static void drawGame(void);
 static void updateGame(void);
 static void loadTexture(void);
 static void unloadTexture(void);
+static void loadSound(void);
+static void unloadSound(void);
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -103,7 +118,9 @@ static void unloadTexture(void);
 int main()
 {
     InitWindow(screenWidth, screenHeight, "Flappy Bird");
+    InitAudioDevice();
     loadTexture();
+    loadSound();
     SetTargetFPS(60);
     InitGame();
     while (!WindowShouldClose())
@@ -112,6 +129,8 @@ int main()
         drawGame();
     }
     unloadTexture();
+    unloadSound();
+    CloseAudioDevice();
     CloseWindow();
 
     return 0;
@@ -123,7 +142,13 @@ int main()
 
 void InitGame(void)
 {
+    gameStart = true;
     gameOver = false;
+    gameRun = 0;
+
+    score = 0;
+    hiScore = 0;
+    speed = 3.0;
 
     map.backgroundY = -500;
     map.foregroundY = 630;
@@ -189,7 +214,7 @@ void jump(void)
     else
     {
         bird.acceleration += bird.gravity * GetFrameTime();
-        bird.rotation ++;
+        bird.rotation++;
     }
 
     if (bird.acceleration >= bird.gravity) bird.acceleration = bird.gravity;
@@ -208,44 +233,73 @@ void drawGame(void)
         DrawTextureEx(map.background, (Vector2) {(float) map.background.width * 2 + map.scrollingBack, map.backgroundY},
                       0.0f, 2.5f, WHITE);
 
-        for (int i = 0; i < MAX_PIPES; i++)
-        {
-            DrawTextureEx(pipe[i].topPipe, (Vector2) {pipe[i].x, pipe[i].topY}, 0.0f, 2.5f, WHITE);
-            DrawTextureEx(pipe[i].bottomPipe, (Vector2) {pipe[i].x, pipe[i].bottomY}, 0.0f, 2.5f, WHITE);
-
-//            DrawRectangle(pipe[i].topPipeRec.x, pipe[i].topPipeRec.y, topPipe_frameWidth, topPipe_frameHeight, BLUE);
-//            DrawRectangle(pipe[i].bottomPipeRec.x, pipe[i].bottomPipeRec.y, bottomPipe_frameWidth, bottomPipe_frameHeight, MAROON);
+        if(gameStart && gameRun == 0) {
+            DrawTextureEx(title, (Vector2) {screenWidth / 4.5, screenHeight / 4}, 0.0f, 3.0f, WHITE);
+            DrawTextureEx(map.foreground, (Vector2) {map.scrollingFore, map.foregroundY}, 0.0f, 2.5f, WHITE);
+            DrawTextureEx(map.foreground,
+                          (Vector2) {(float) map.foreground.width * 2 + map.scrollingFore, map.foregroundY},
+                          0.0f, 2.5f, WHITE);
+            DrawTexturePro(bird.birdSprite,
+                           (Rectangle) {currentFrame * bird.frameWidth, 0, bird.frameWidth, bird.birdSprite.height},
+                           (Rectangle) {bird.x + (bird.x / 3), bird.y, bird.frameWidth, bird.birdSprite.height},
+                           (Vector2) {bird.frameWidth, bird.birdSprite.height}, bird.rotation, WHITE);
+            DrawText(TextFormat("Press SPACEBAR to jump"), GetScreenWidth()/2 - MeasureText(TextFormat("Press ENTER to restart"), 15)/2, screenHeight/2 + 50, 15, BLACK);
         }
 
-        DrawTextureEx(map.foreground, (Vector2) {map.scrollingFore, map.foregroundY}, 0.0f, 2.5f, WHITE);
-        DrawTextureEx(map.foreground, (Vector2) {(float) map.foreground.width * 2 + map.scrollingFore, map.foregroundY},
-                      0.0f, 2.5f, WHITE);
+        else if(gameRun == 1)
+        {
+            for (int i = 0; i < MAX_PIPES; i++)
+            {
+                DrawTextureEx(pipe[i].topPipe, (Vector2) {pipe[i].x, pipe[i].topY}, 0.0f, 2.5f, WHITE);
+                DrawTextureEx(pipe[i].bottomPipe, (Vector2) {pipe[i].x, pipe[i].bottomY}, 0.0f, 2.5f, WHITE);
 
-        DrawTexturePro(bird.birdSprite,(Rectangle) {currentFrame * bird.frameWidth, 0, bird.frameWidth, bird.birdSprite.height},
-                                         (Rectangle) {bird.x + (bird.x / 3), bird.y, bird.frameWidth, bird.birdSprite.height},
-                                           (Vector2) {bird.frameWidth, bird.birdSprite.height}, bird.rotation, WHITE);
+//                DrawRectangle(pipe[i].topPipeRec.x, pipe[i].topPipeRec.y, topPipe_frameWidth, topPipe_frameHeight, BLUE);
+//                DrawRectangle(pipe[i].bottomPipeRec.x, pipe[i].bottomPipeRec.y, bottomPipe_frameWidth, bottomPipe_frameHeight, MAROON);
+            }
 
-//        DrawRectangle(bird.x+5, bird.y - bird.birdSprite.height+15, bird.frameWidth-10, bird.birdSprite.height-10, RED);
+            DrawTextureEx(map.foreground, (Vector2) {map.scrollingFore, map.foregroundY}, 0.0f, 2.5f, WHITE);
+            DrawTextureEx(map.foreground,
+                          (Vector2) {(float) map.foreground.width * 2 + map.scrollingFore, map.foregroundY},
+                          0.0f, 2.5f, WHITE);
+
+            DrawTexturePro(bird.birdSprite,
+                           (Rectangle) {currentFrame * bird.frameWidth, 0, bird.frameWidth, bird.birdSprite.height},
+                           (Rectangle) {bird.x + (bird.x / 3), bird.y, bird.frameWidth, bird.birdSprite.height},
+                           (Vector2) {bird.frameWidth, bird.birdSprite.height}, bird.rotation, WHITE);
+
+//            DrawRectangle(bird.x+5, bird.y - bird.birdSprite.height+15, bird.frameWidth-10, bird.birdSprite.height-10, RED);
 //
-//        DrawRectangle(bird.x, map.foregroundY, bird.frameWidth-10, map.foreground.height, PURPLE);
-//        DrawRectangle(bird.x, -50, bird.frameWidth-10, map.foreground.height, PURPLE);
+//            DrawRectangle(bird.x, map.foregroundY, bird.frameWidth-10, map.foreground.height, PURPLE);
+//            DrawRectangle(bird.x, -50, bird.frameWidth-10, map.foreground.height, PURPLE);
 
-        DrawText(TextFormat("Score = %d",score),5,5,15,BLACK);
-        DrawText(TextFormat("Hi-Score = %d",hiScore),5,25,15,BLACK);
+            DrawText(TextFormat("Score %d", score), 5, 5, 20, BLACK);
+            DrawText(TextFormat("Hi-Score %d", hiScore), 5, 30, 20, BLACK);
+        }
     }
     else
     {
         DrawTextureEx(map.background, (Vector2) {map.scrollingBack, map.backgroundY}, 0.0f, 2.5f, WHITE);
         DrawTextureEx(map.background, (Vector2) {(float) map.background.width * 2 + map.scrollingBack, map.backgroundY},0.0f, 2.5f, WHITE);
-        DrawTextureEx(gameOverSprite, (Vector2) {screenWidth/5,screenHeight/4}, 0.0f, 3.0f, WHITE);
+
+        for (int i = 0; i < MAX_PIPES; i++)
+        {
+            DrawTextureEx(pipe[i].topPipe, (Vector2) {pipe[i].x, pipe[i].topY}, 0.0f, 2.5f, WHITE);
+            DrawTextureEx(pipe[i].bottomPipe, (Vector2) {pipe[i].x, pipe[i].bottomY}, 0.0f, 2.5f, WHITE);
+        }
+
+        DrawTextureEx(map.foreground, (Vector2) {map.scrollingFore, map.foregroundY}, 0.0f, 2.5f, WHITE);
+        DrawTextureEx(map.foreground, (Vector2) {(float) map.foreground.width * 2 + map.scrollingFore, map.foregroundY},0.0f, 2.5f, WHITE);
+
         DrawTexturePro(bird.birdSprite,(Rectangle) {currentFrame * bird.frameWidth, 0, bird.frameWidth, bird.birdSprite.height},
                        (Rectangle) {bird.x + (bird.x / 3), bird.y, bird.frameWidth, bird.birdSprite.height},
                        (Vector2) {bird.frameWidth, bird.birdSprite.height}, bird.rotation, WHITE);
-        DrawTextureEx(scoreBoard, (Vector2) {screenWidth/5,screenHeight/3 + 15}, 0.0f, 2.5f, WHITE);
-        DrawText(TextFormat("%d",score),GetScreenWidth()/2 - MeasureText(TextFormat("%d",score),20)/2,screenHeight/3 + 60,25, BLACK);
-        DrawText(TextFormat("%d",hiScore),GetScreenWidth()/2 - MeasureText(TextFormat("%d",hiScore),20)/2, screenHeight/3 + 115,2, BLACK);
-        DrawTextureEx(map.foreground, (Vector2) {map.scrollingFore, map.foregroundY}, 0.0f, 2.5f, WHITE);
-        DrawTextureEx(map.foreground, (Vector2) {(float) map.foreground.width * 2 + map.scrollingFore, map.foregroundY},0.0f, 2.5f, WHITE);
+
+        DrawTextureEx(gameOverSprite, (Vector2) {screenWidth/5,screenHeight/4}, 0.0f, 3.0f, WHITE);
+
+        DrawTextureEx(scoreBoard, (Vector2) {screenWidth/5 + 2,screenHeight/3 + 15}, 0.0f, 2.5f, WHITE);
+        DrawText(TextFormat("%d",score),GetScreenWidth()/2 - MeasureText(TextFormat("%d",score),25)/2,screenHeight/3 + 60,25, BLACK);
+        DrawText(TextFormat("%d",hiScore),GetScreenWidth()/2 - MeasureText(TextFormat("%d",hiScore),25)/2, screenHeight/3 + 115,25, BLACK);
+        DrawText(TextFormat("Press ENTER to restart"), GetScreenWidth()/2 - MeasureText(TextFormat("Press ENTER to restart"), 15)/2, screenHeight/2 + 50, 15, BLACK);
     }
     EndDrawing();
 }
@@ -257,14 +311,15 @@ void updateGame(void)
     if (map.scrollingBack <= -(float) map.background.width * 2) map.scrollingBack = 0;
     if (map.scrollingFore <= -(float) map.foreground.width * 2) map.scrollingFore = 0;
 
+    Rectangle birdRec = {bird.x + 5, bird.y - bird.birdSprite.height + 15, bird.frameWidth - 10,bird.birdSprite.height - 10};
+    Rectangle topRec = {bird.x, -50, bird.frameWidth-10, map.foreground.height};
+    Rectangle bottomRec = {bird.x, map.foregroundY, bird.frameWidth-10, map.foreground.height};
+
     if (!gameOver)
     {
-        Rectangle birdRec = {bird.x + 5, bird.y - bird.birdSprite.height + 15, bird.frameWidth - 10,bird.birdSprite.height - 10};
-        Rectangle topRec = {bird.x, -50, bird.frameWidth-10, map.foreground.height};
-        Rectangle bottomRec = {bird.x, map.foregroundY, bird.frameWidth-10, map.foreground.height};
-
         map.framesCounter++;
-        if (map.framesCounter >= (60 / map.framesSpeed)) {
+        if (map.framesCounter >= (60 / map.framesSpeed))
+        {
             map.framesCounter = 0;
             currentFrame++;
 
@@ -273,39 +328,61 @@ void updateGame(void)
             birdRec.x = (float) currentFrame * (float) bird.birdSprite.width / 3;
         }
 
-        if (!gameOver && IsKeyPressed(KEY_SPACE))bird.isJumping = 1;
+        if (!gameOver && IsKeyPressed(KEY_SPACE))
+        {
+            bird.isJumping = 1;
+            PlaySound(effect.jump);
+        }
 
         if (bird.isJumping == 1)
         {
-            for (int i = 0; i < MAX_PIPES; i++){
-                pipe[i].x -= 3;
-                pipe[i].topPipeRec.x -= 3;
-                pipe[i].bottomPipeRec.x -=3;
+            gameRun = 1;
+            for (int i = 0; i < MAX_PIPES; i++)
+            {
+                pipe[i].x -= speed;
+                pipe[i].topPipeRec.x -= speed;
+                pipe[i].bottomPipeRec.x -= speed;
             }
             if (bird.y < map.ground && bird.y > map.ceiling) jump();
         }
 
-        if (CheckCollisionRecs(birdRec, topRec) || CheckCollisionRecs(birdRec, bottomRec)) gameOver = true;
+        if (CheckCollisionRecs(birdRec, topRec) || CheckCollisionRecs(birdRec, bottomRec))
+        {
+            PlaySound(effect.hit);
+            gameOver = true;
+        }
 
         for(int i = 0; i < MAX_PIPES; i++)
         {
-            if ((CheckCollisionRecs(birdRec, pipe[i].topPipeRec) || CheckCollisionRecs(birdRec, pipe[i].bottomPipeRec)) && pipe[i].active) gameOver = true;
+            if ((CheckCollisionRecs(birdRec, pipe[i].topPipeRec) || CheckCollisionRecs(birdRec, pipe[i].bottomPipeRec)) && pipe[i].active)
+            {
+                PlaySound(effect.hit);
+                gameOver = true;
+            }
             else if ((pipe[i].topPipeRec.x + pipe[i].topPipeRec.width < birdRec.x) && (pipe[i].bottomPipeRec.x + pipe[i].bottomPipeRec.width< bird.x) && !gameOver && pipe[i].active)
             {
+                PlaySound(effect.point);
                 score++;
                 pipe[i].active = false;
             }
         }
+
+        if (score % 5 == 0 && score != 0) speed += 0.005f;
         if (score > hiScore) hiScore = score;
     }
     else
     {
-        if (IsKeyPressed(KEY_ENTER))
-        {
-            InitGame();
-            score = 0;
-            gameOver = false;
-        }
+        if (bird.rotation <= 30) bird.rotation += 4;
+        bird.acceleration += bird.gravity * GetFrameTime();
+
+        if (bird.acceleration >= bird.gravity) bird.acceleration = bird.gravity;
+
+        bird.velocity += bird.acceleration * GetFrameTime() * 10;
+        bird.y += bird.velocity * GetFrameTime() * 5;
+
+        if (CheckCollisionRecs(birdRec, bottomRec)) bird.y = bottomRec.y + 15;
+
+        if (IsKeyPressed(KEY_ENTER)) InitGame();
     }
 }
 
@@ -318,6 +395,7 @@ void loadTexture(void)
     static const char bottomPipePath[] = "/Users/neneprm/Desktop/C/FlappyBird/assets/bottomPipe.png";
     static const char gameOverPath[] = "/Users/neneprm/Desktop/C/FlappyBird/assets/gameOver.png";
     static const char scoreBoardPath[] = "/Users/neneprm/Desktop/C/FlappyBird/assets/scoreBoard.png";
+    static const char titlePath[] = "/Users/neneprm/Desktop/C/FlappyBird/assets/title.png";
 
     map.background = LoadTexture(backgroundPath);
     map.foreground = LoadTexture(foregroundPath);
@@ -329,7 +407,7 @@ void loadTexture(void)
     }
     gameOverSprite = LoadTexture(gameOverPath);
     scoreBoard = LoadTexture(scoreBoardPath);
-
+    title = LoadTexture(titlePath);
 }
 
 void unloadTexture(void)
@@ -344,4 +422,23 @@ void unloadTexture(void)
     }
     UnloadTexture(gameOverSprite);
     UnloadTexture(scoreBoard);
+    UnloadTexture(title);
+}
+
+void loadSound(void)
+{
+    static const char hitPath[] = "/Users/neneprm/Desktop/C/FlappyBird/effect/hit.mp3";
+    static const char jumpPath[] = "/Users/neneprm/Desktop/C/FlappyBird/effect/jump.mp3";
+    static const char pointPath[] = "/Users/neneprm/Desktop/C/FlappyBird/effect/point.mp3";
+
+    effect.hit = LoadSound(hitPath);
+    effect.jump = LoadSound(jumpPath);
+    effect.point = LoadSound(pointPath);
+}
+
+void unloadSound(void)
+{
+    UnloadSound(effect.hit);
+    UnloadSound(effect.jump);
+    UnloadSound(effect.point);
 }
